e9a86220dc790489efe92216e48729580d1db876
adelson70 <129080230+adelson70@users.noreply.github.com>
2024-02-28 18:36:58 -0300

    add fonts

--- nps_user.py
+++ nps_user.py
@@ -1,22 +1,34 @@
 import tkinter as tk
+from tkinter.font import Font
 
 def avaliacao(nivel_satisfacao):
-    print("Nível de satisfação selecionado:", nivel_satisfacao)
+    return saida_info(nivel_satisfacao)
+
+def saida_info(satisfacao):
+
+    return ...
 
 # GUI
 janela = tk.Tk()
 janela.title('Avaliação de Satisfação')
-janela.geometry('380x80')
+janela.geometry('370x80')
+
+# Fonts
+fonte_titulo = Font(janela, family='Arial', weight='normal')
+fonte_botoes = Font(janela, family='Arial', weight='normal')
+
+# Bloqueando o redimensionamento
+janela.resizable(width=False, height=False)
 
 # Labels
-titulo = tk.Label(janela, text='Avalie sua Experiência')
+titulo = tk.Label(janela, text='Avalie sua Experiência', font=fonte_titulo)
 titulo.pack(pady=5)
 
 # Buttons
 botoes_satisfacao = []
 for i in range(1, 11):
-    botao = tk.Button(janela, text=str(i), command=lambda nivel=i: avaliacao(nivel))
+    botao = tk.Button(janela, text=str(i), font=fonte_botoes, command=lambda nivel=i: avaliacao(nivel))
     botoes_satisfacao.append(botao)
-    botao.pack(side=tk.LEFT, padx=10, pady=5)  # Adicionando os botões lado a lado
+    botao.pack(side=tk.LEFT, padx=4, pady=5)  # Adicionando os botões lado a lado
 
 janela.mainloop()
