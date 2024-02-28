8b1e5fb62d22d1441b16b324cff6dac719c365e4
adelson70 <129080230+adelson70@users.noreply.github.com>
2024-02-28 13:22:28 -0300

    esqueleto

--- nps_user.py
+++ nps_user.py
@@ -1,9 +1,22 @@
-# Importando bibliotecas
 import tkinter as tk
 
+def avaliacao(nivel_satisfacao):
+    print("Nível de satisfação selecionado:", nivel_satisfacao)
 
-
+# GUI
 janela = tk.Tk()
 janela.title('Avaliação de Satisfação')
+janela.geometry('400x200')
+
+# Labels
+titulo = tk.Label(janela, text='Avalie sua Experiência')
+titulo.pack(pady=5)
+
+# Buttons
+botoes_satisfacao = []
+for i in range(1, 11):
+    botao = tk.Button(janela, text=str(i), command=lambda nivel=i: avaliacao(nivel))
+    botoes_satisfacao.append(botao)
+    botao.pack(side=tk.LEFT, padx=1, pady=5)  # Adicionando os botões lado a lado
 
 janela.mainloop()
