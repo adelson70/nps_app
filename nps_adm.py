31c23387e47a32827b635a3d56dc29a1bc639316
adelson70 <129080230+adelson70@users.noreply.github.com>
2024-03-01 12:58:16 -0300

    add labels

--- nps_adm.py
+++ nps_adm.py
@@ -7,6 +7,17 @@ import sqlite3 as sql
 # GUI
 janela = tk.Tk()
 janela.title('Avaliações')
-janela.geometry('350x220')
+janela.geometry('240x90')
+janela.resizable(width=False, height=False)
+
+# Fontes personalizadas
+fonte_status = Font(janela, family='Arial', weight='normal', size=20)
+
+# Labels
+status = tk.Label(janela, text='BOM', font=fonte_status)
+status.pack(pady=2)
+
+valor_nps = tk.Label(janela, text='NPS: 70%', font=fonte_status)
+valor_nps.pack(pady=3)
 
 janela.mainloop()
